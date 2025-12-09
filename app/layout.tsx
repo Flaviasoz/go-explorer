2aa3f80a7eaad5a9e8ee36e5aed7702ad8405f73
Flaviasoz <flaviaatur@gmail.com>
2025-12-09 18:00:51 -0300

    atualização config de formatador

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,33 +1,34 @@
-import React from "react";
-import "@/core/css/globals.css";
-import type { Metadata } from "next";
-import { Elms_Sans, Quicksand } from "next/font/google";
+import React from 'react'
+import '@/core/css/globals.css'
+import type { Metadata } from 'next'
+import { Elms_Sans, Quicksand } from 'next/font/google'
 
 const elmsSans = Elms_Sans({
-  variable: "--font-elms-sans",
-  subsets: ["latin"]
-});
+  variable: '--font-elms-sans',
+  subsets: ['latin']
+})
 
 const quicksand = Quicksand({
-  variable: "--font-quicksand",
-  subsets: ["latin"]
-});
+  variable: '--font-quicksand',
+  subsets: ['latin']
+})
 
 export const metadata: Metadata = {
-  title: "Go Explorer - Pacotes de Viagens e Roteiros Personalizados",
-  description: "Descubra os melhores pacotes de viagens, guias de turismo e roteiros personalizados para sua próxima aventura. Planeje sua viagem dos sonhos com a Go Explorer."
-};
+  title: 'Go Explorer - Pacotes de Viagens e Roteiros Personalizados',
+  description:
+    'Descubra os melhores pacotes de viagens, guias de turismo e roteiros personalizados para sua próxima aventura. Planeje sua viagem dos sonhos com a Go Explorer.'
+}
 
-const RootLayout = ({ children, }: Readonly<{ children: React.ReactNode; }>) => {
+const RootLayout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
   return (
-    <html lang="pt-BR">
+    <html lang='pt-BR'>
       <body
         className={`${elmsSans.variable} ${quicksand.variable} antialiased`}
       >
         {children}
       </body>
     </html>
-  );
-};
+  )
+}
 
-export default RootLayout;
+export default RootLayout
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,38 +1,52 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from "eslint/config"
+import nextVitals from "eslint-config-next/core-web-vitals"
+import nextTs from "eslint-config-next/typescript"
+import prettier from "eslint-config-prettier/flat"
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  globalIgnores([
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+  prettier,
+  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
   {
     rules: {
+      "@typescript-eslint/no-explicit-any": "error",
       "no-multiple-empty-lines": ["error", { max: 1, maxEOF: 0, maxBOF: 0 }],
       "no-trailing-spaces": "error",
       "eol-last": ["error", "always"],
+      indent: [
+        "error",
+        2,
+        {
+          SwitchCase: 1,
+          ignoredNodes: []
+        }
+      ],
+      quotes: [
+        "error",
+        "single",
+        {
+          avoidEscape: true,
+          allowTemplateLiterals: true
+        }
+      ],
       "no-multi-spaces": "error",
-      "object-curly-spacing": ["error", "always"],
-      "array-bracket-spacing": ["error", "never"],
-      "comma-spacing": ["error", { before: false, after: true }],
-      "key-spacing": ["error", { beforeColon: false, afterColon: true }],
-      "space-before-blocks": "error",
-      "space-before-function-paren": ["error", {
-        "anonymous": "always",
-        "named": "never",
-        "asyncArrow": "always"
-      }],
-      "space-in-parens": ["error", "never"],
-      "space-infix-ops": "error",
-      "space-unary-ops": "error",
-      "spaced-comment": ["error", "always"],
-    },
-  },
-]);
+      "max-len": [
+        "error",
+        {
+          code: 80,
+          tabWidth: 2,
+          ignoreUrls: true,
+          ignoreComments: false,
+          ignoreStrings: true,
+          ignoreTemplateLiterals: true
+        }
+      ],
+      semi: ["error", "never"],
+      "no-extra-semi": "error",
+      "comma-dangle": ["error", "never"]
+    }
+  }
+])
 
-export default eslintConfig;
+export default eslintConfig
