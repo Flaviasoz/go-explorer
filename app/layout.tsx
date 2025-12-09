917f3ecee04e6b57060e1994abf9b43ac4b87ba5
Flaviasoz <flaviaatur@gmail.com>
2025-12-09 16:57:15 -0300

    att

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,31 +1,33 @@
+import React from "react";
+import "@/core/css/globals.css";
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import { Elms_Sans, Quicksand } from "next/font/google";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+const elmsSans = Elms_Sans({
+  variable: "--font-elms-sans",
+  subsets: ["latin"]
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const quicksand = Quicksand({
+  variable: "--font-quicksand",
+  subsets: ["latin"]
 });
 
 export const metadata: Metadata = {
   title: "Go Explorer - Pacotes de Viagens e Roteiros Personalizados",
-  description: "Descubra os melhores pacotes de viagens, guias de turismo e roteiros personalizados para sua próxima aventura. Planeje sua viagem dos sonhos com a Go Explorer.",
+  description: "Descubra os melhores pacotes de viagens, guias de turismo e roteiros personalizados para sua próxima aventura. Planeje sua viagem dos sonhos com a Go Explorer."
 };
 
-const RootLayout = ({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) => {
+
+
+
+
+
+const RootLayout = ({ children, }: Readonly<{ children: React.ReactNode; }>) => {
   return (
     <html lang="pt-BR">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${elmsSans.variable} ${quicksand.variable} antialiased`}
       >
         {children}
       </body>
